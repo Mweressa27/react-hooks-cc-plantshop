bae2842b65c935243acb83a4937edb814cb562b2
Mweresa Esther Magoma <esthermweresa27@gmail.com>
2025-04-20 17:55:33 +0300

    complete tests

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,18 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PlantCard() {
+function PlantCard({plant, onToggleSoldOut, onUpdatePrice, onDeletePlant}) {
+  const [newPrice, setNewPrice] = useState(plant.price)
+
+  function handlePriceChange(e) {
+    setNewPrice(e.target.value);
+  }
+
+  function handlePriceSubmit(e) {
+    e.preventDefault();
+    onUpdatePrice(plant.id, newPrice);
+  }
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
-      ) : (
-        <button>Out of Stock</button>
-      )}
-    </li>
-  );
+    <img src={plant.image} alt={plant.name} />
+    <h4>{plant.name}</h4>
+    <p>Price: {plant.price}</p>
+
+    <form onSubmit={handlePriceSubmit}>
+      <input
+        type="number"
+        step="0.01"
+        value={newPrice}
+        onChange={handlePriceChange}
+      />
+      <button type="submit">Update Price</button>
+    </form>
+
+    {plant.isSoldOut ? (
+      <button onClick={() => onToggleSoldOut(plant.id)}>Out of Stock</button>
+    ) : (
+      <button className="primary" onClick={() => onToggleSoldOut(plant.id)}>
+        In Stock
+      </button>
+    )}
+
+    <button onClick={() => onDeletePlant(plant.id)}>Delete</button>
+  </li>
+);
 }
 
 export default PlantCard;
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((res) => res.json())
+      .then(setPlants);
+  }, []);
+
+  function handleAddPlant(newPlant) {
+    setPlants([...plants, newPlant]);
+  }
+
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  function handleToggleSoldOut(id) {
+    const updatedPlants = plants.map((plant) => 
+    plant.id === id ? {...plant, isSoldOut: !plant.isSoldOut} : plant)
+    setPlants(updatedPlants)
+  }
+
+  function handleUpdatePrice(id, newPrice) {
+    fetch(`http://localhost:6001/plants/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ price: newPrice })
+    })
+      .then((res) => res.json())
+      .then((updatedPlant) => {
+        const updatedPlants = plants.map((plant) =>
+          plant.id === updatedPlant.id ? updatedPlant : plant
+        );
+        setPlants(updatedPlants);
+      });
+  }
+  
+  function handleDeletePlant(id) {
+    fetch(`http://localhost:6001/plants/${id}`, {
+      method: "DELETE"
+    }).then(() => {
+      const updatedPlants = plants.filter((plant) => plant.id !== id);
+      setPlants(updatedPlants);
+    });
+  }
+  
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search searchTerm={searchTerm} onSearchChange={setSearchTerm} />
+      <PlantList plants={filteredPlants} onToggleSoldOut={handleToggleSoldOut} onUpdatePrice={handleUpdatePrice}
+  onDeletePlant={handleDeletePlant} />
     </main>
   );
 }
